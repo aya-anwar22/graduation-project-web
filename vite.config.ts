a879e5ac0442c4d5470bed73b5b07946ef37af8f
ya-anwar22 <ayaanwar.se@gmail.com>
2026-07-08 16:49:10 +0300

    sonarcloud

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,17 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
+  plugins: [
+    react(),
     tailwindcss()
   ],
+
+  test: {
+    coverage: {
+      reporter: [
+        'text',
+        'lcov'
+      ]
+    }
+  }
 })
